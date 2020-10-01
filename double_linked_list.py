0e9be7913657c9ff9291d195e0eed620f4caebd6
stevekutz <stkutz@gmail.com>
2020-10-01 14:47:32 -0500

    refactored using is None instead of using equality operators-per PEP 8 Styling Guide

--- double_linked_list.py
+++ double_linked_list.py
@@ -11,9 +11,9 @@ class DoubleLinkedList:
     def add_to_head(self, value):
 
         # List is empty
-        if self.head == None:
+        if self.head is None:
             new_node = Node(value)
-            new_node.next = self.head
+            # new_node.next = self.head
             self.head = new_node   
         # nodes already exist, must manage prev and next
         else:
@@ -23,15 +23,22 @@ class DoubleLinkedList:
             orig_head.prev = new_node  # point to new node
             self.head = new_node   # assign new node as head
 
+            # # another way without defining a variable to clarify orig_head
+            # new_node = Node(value)
+            # new_node.next = self.head
+            # self.head.prev = new_node
+            # self.head = new_node
+
+
     def add_to_tail(self, value):
 
         current = self.head
         last_node = current
 
         # if list empty
-        if current == None: 
+        if current is None: 
             new_node = Node(value)
-            new_node.next = current
+            # new_node.next = current
             self.head = new_node
             return
 
@@ -49,7 +56,7 @@ class DoubleLinkedList:
         current = self.head
         list_str = 'HEAD >> '
 
-        if current == None:
+        if current is None:
             print(f' Double Linked List is empty ')
 
         while current:
@@ -64,21 +71,22 @@ class DoubleLinkedList:
 
 
 
+
 dbl_ll_1 = DoubleLinkedList()
 
-# dbl_ll_1.add_to_head(3)
+dbl_ll_1.add_to_head(3)
+dbl_ll_1.print()
+
+dbl_ll_1.add_to_head(2)
+dbl_ll_1.add_to_head(1)
+dbl_ll_1.print()
+
+# dbl_ll_1.add_to_tail(10)
 # dbl_ll_1.print()
 
-# dbl_ll_1.add_to_head(2)
-# dbl_ll_1.add_to_head(1)
+# dbl_ll_1.add_to_tail(11)
 # dbl_ll_1.print()
 
-dbl_ll_1.add_to_tail(10)
-dbl_ll_1.print()
-
-dbl_ll_1.add_to_tail(11)
-dbl_ll_1.print()
-
-dbl_ll_1.add_to_tail(12)
-dbl_ll_1.add_to_tail(13)
-dbl_ll_1.print()
+# dbl_ll_1.add_to_tail(12)
+# dbl_ll_1.add_to_tail(13)
+# dbl_ll_1.print()
